07dcd33e9c12e64905b033e5f7c9568467064b67
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-02 19:07:49 +0200

    creato scheletro html e js

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+'use strict';
+// prima di tutto dichiariamo delle variabili con selezionati gli elementi che ci interessano
+const containerImmagineLampadina = document.querySelector('#lampadinaContainer')
+const interactiveButton = document.querySelector('#interactiveButton')
+
+
