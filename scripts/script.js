76d3fb57a3eee7091719327de35f994ea896edb0
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-02 20:39:45 +0200

    modificato un po' lo stile e aggiunti commenti in js

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,18 @@
 'use strict';
-// prima di tutto dichiariamo e assegnamo delle variabili con selezionati gli elementi che ci interessano
+
+////////////////////
+// VARIABILI GLOBALI
+////////////////////
 const containerImmagineLampadina = document.querySelector('#lampadinaContainer')
 const bottoneInterattivo = document.querySelector('#interactiveButton')
+const sfondoSpento = document.querySelector('.wrapper')
 
-// dobbiamo sostituire l'immagine nell'html della lampadina spenta
-// con quella accesa con .innerHTML quando si clicca il bottone
 
+// funzione che al click del bottone cambia immagine, testo del bottone e sfondo del container
 function clickButtonToSwitchLampOnOff() {
-    const bottoneSpento = 'Spegni Lampadina';
+
+    // VARIABILI NELLO SCOPE DELLA FUNZIONE
+    const bottoneSpento = 'Spegni Lampadina'; // dichiarata variabile e assegnata con testo per il bottone
     const bottoneAcceso = 'Accendi Lampadina';
     const imgLampadaAccesa = `
     <img src="./img/yellow_lamp.png" alt="lampadina accesa"></img>
@@ -15,20 +20,27 @@ function clickButtonToSwitchLampOnOff() {
     const imgLampadinaSpenta = `
     <img src="./img/white_lamp.png" alt="lampadina spenta"></img>
     `;
-    if (bottoneInterattivo.innerHTML.includes('Accendi')) {
-        bottoneInterattivo.innerHTML = bottoneSpento;
 
-        containerImmagineLampadina.innerHTML = imgLampadaAccesa;
-    } else {
+    // questo if fa un controllo nel testo all'interno del pulsante nell'HTML 
+    // quindi se è presento quel testo farà le sue azioni
+
+    if (bottoneInterattivo.innerHTML.includes('Accendi')) {
+
+        bottoneInterattivo.innerHTML = bottoneSpento; // sostituirà il testo all'interno del bottono
+        containerImmagineLampadina.innerHTML = imgLampadaAccesa; // sostituirà l'immagine
+
+        sfondoSpento.style.backgroundColor = 'white'; // cambierà lo sfondo nell'HTML 
+        sfondoSpento.style.color = 'black';
+
+    } else if (bottoneInterattivo.innerHTML.includes('Spegni')) {
         bottoneInterattivo.innerHTML = bottoneAcceso;
 
         containerImmagineLampadina.innerHTML = imgLampadinaSpenta;
+        sfondoSpento.style.backgroundColor = 'black';
+        sfondoSpento.style.color = 'white';
     }
 }
 
 // adesso il bottone deve avere un eventlistener
 // per far si che quando lo si clicca ci sia un evento che scateni la funzione
 bottoneInterattivo.addEventListener('click', clickButtonToSwitchLampOnOff);
-
-// adesso facciamo dobbiamo far spegnere la lampadina e fare l'inverso di prima
-
