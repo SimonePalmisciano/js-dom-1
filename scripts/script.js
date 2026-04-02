7afdf3a2c8420a29584649b26700e5c4d912c893
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-02 20:00:47 +0200

    create varibili che contengono gli elementi necessari per far avvenire la magia

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,18 @@
 'use strict';
-// prima di tutto dichiariamo delle variabili con selezionati gli elementi che ci interessano
+// prima di tutto dichiariamo e assegnamo delle variabili con selezionati gli elementi che ci interessano
 const containerImmagineLampadina = document.querySelector('#lampadinaContainer')
-const interactiveButton = document.querySelector('#interactiveButton')
+const bottoneInterattivo = document.querySelector('#interactiveButton')
 
+// dobbiamo sostituire l'immagine nell'html della lampadina spenta
+// con quella accesa con .innerHTML quando si clicca il bottone
+
+function clickButtonToSwitchLampOnOff() {
+
+}
+
+// adesso il bottone deve avere un eventlistener
+// per far si che quando lo si clicca ci sia un evento che scateni la funzione
+bottoneInterattivo.addEventListener('click', clickButtonToSwitchLampOnOff);
+
+// adesso facciamo dobbiamo far spegnere la lampadina e fare l'inverso di prima
 
